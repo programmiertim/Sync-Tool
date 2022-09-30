e83bd2ce9e1bf229e28d77653c61f24973d4c1bf
Tim Albert <info@timalbert.de>
2022-09-30 10:56:58 +0200

    Initial commit

---
+++ src/main/java/tools/albert/synctool/SyncToolApplication.java
@@ -0,0 +1,13 @@
+package tools.albert.synctool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tools/albert/synctool/SyncToolApplicationTests.java
@@ -0,0 +1,13 @@
+package tools.albert.synctool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
